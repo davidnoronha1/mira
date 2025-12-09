03f43addbca348440c393a9a34edcaad62c018d5
Shaurya <shaurya@example.com>
2025-12-09 23:37:38 +0530

    updated joystick controls

--- src/mira2_rov/include/mira2_rov/joystick_utils.hpp
+++ src/mira2_rov/include/mira2_rov/joystick_utils.hpp
@@ -31,21 +31,20 @@ public:
 
 private:
     void joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg) {
-        int pitch_button = msg->buttons[5];
-        int mode_stabilise = msg->buttons[1];
-        int mode_acro = msg->buttons[7];
-        int mode_manual = msg->buttons[3];
-        int yaw_hold_button = msg->buttons[6];
-        int pitch_up_button = msg->buttons[4];
         prev_msg_ = arm_disarm_;
         arm_disarm_ = msg->buttons[0];
+        int mode_manual = msg->buttons[1];
+        int mode_depth_hold = msg->buttons[2];
+        int mode_stabilise = msg->buttons[3];
 
-        msg_to_pub_.pitch = 1500;
-        msg_to_pub_.roll = 1500 + (((msg->buttons[4]) * -400) + ((msg->buttons[5]) * 400)) * sensitivity_all_axes_;
+        int roll_left_button = msg->buttons[4];
+        int roll_right_button = msg->buttons[5];
+
+        msg_to_pub_.pitch = 1500 + ((msg->axes[4]) * 400) * sensitivity_all_axes_;
         msg_to_pub_.thrust = 1500 + ((((msg->axes[5]) + 1) * -200) + (((msg->axes[2]) + 1) * 200)) * sensitivity_all_axes_;
-        msg_to_pub_.forward = 1500 + ((msg->axes[4]) * 400) * 1; // sensitivity_all_axes_;
-        msg_to_pub_.lateral = 1500 + ((msg->axes[3]) * -400) * sensitivity_all_axes_;
-        msg_to_pub_.yaw = 1500 + (((msg->axes[0]) * -400)) * sensitivity_yaw_;
+        msg_to_pub_.forward = 1500 + ((msg->axes[1]) * 400) * sensitivity_all_axes_;
+        msg_to_pub_.lateral = 1500 + ((msg->axes[0]) * -400) * sensitivity_all_axes_;
+        msg_to_pub_.yaw = 1500 + (((msg->axes[3]) * -400)) * sensitivity_yaw_;
 
         if (arm_disarm_ == 1 && prev_msg_ == 0) {
             if (msg_to_pub_.arm == 0) {
@@ -57,21 +56,21 @@ private:
             }
         }
 
-        if (pitch_button == 1) {
-            msg_to_pub_.pitch = 1400;
-        } else if (pitch_up_button == 1) {
-            msg_to_pub_.pitch = 1600;
+        if (roll_left_button == 1) {
+            msg_to_pub_.roll = 1400;
+        } else if (roll_right_button == 1) {
+            msg_to_pub_.roll = 1600;
         } else {
-            msg_to_pub_.pitch = 1500;
+            msg_to_pub_.roll = 1500;
         }
 
         if (mode_stabilise == 1 && msg_to_pub_.mode != "STABILIZE") {
             msg_to_pub_.mode = "STABILIZE";
             RCLCPP_INFO(this->get_logger(), "Mode changed to STABILIZE");
         }
-        if (mode_acro == 1 && msg_to_pub_.mode != "ACRO") {
-            msg_to_pub_.mode = "ACRO";
-            RCLCPP_INFO(this->get_logger(), "Mode changed to ACRO");
+        if (mode_depth_hold == 1 && msg_to_pub_.mode != "ALT_HOLD") {
+            msg_to_pub_.mode = "ALT_HOLD";
+            RCLCPP_INFO(this->get_logger(), "Mode changed to ALT_HOLD");
         }
         if (mode_manual == 1 && msg_to_pub_.mode != "MANUAL") {
             msg_to_pub_.mode = "MANUAL";
