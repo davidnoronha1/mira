459c150bf0e2a4196a6921f60945232a3429c40c
davidnoronha1 <davidnoronha@outlook.in>
2026-03-04 19:33:43 +0530

    migrate vision_bb to onnxruntime and remove vision_depth

---
+++ src/dependencies/vision_boundingbox/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'vision_boundingbox'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'models'), glob('models/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your.email@example.com',
+    description='YOLO-based object detection with ONNX Runtime',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'vision_boundingbox_exe = vision_boundingbox.detection_node:main',
+        ],
+    },
+)
