13a9eee01aae395273f2f14cd2554128c69febab
rena <renamaryanil@gmail.com>
2026-03-20 15:10:28 +0530

    made changes to auto launch file

--- src/mira2_perception/launch/camera_auto.launch.py
+++ src/mira2_perception/launch/camera_auto.launch.py
@@ -1,13 +1,39 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, LogInfo
 
 def generate_launch_description():
-    pkg_launch_dir = os.path.join(get_package_share_directory('mira2_perception'), 'launch')
+    machine = os.environ.get('MACHINE_NAME', 'ORIN')
+    port = "8554" 
     
+    if machine == 'RPI4':
+        usb_path = "usb-3610000.usb-2.1"
+    else:
+        usb_path = "usb-xhci-hcd.1-1"
+
+    dev_path = f"/dev/v4l/by-path/platform-{usb_path}-video-index0"
+
+    camera_node = Node(
+        package='camera_driver',
+        executable='camera_driver_exe',
+        name='camera_auto',
+        output='screen',
+        parameters=[
+            {'port': port},
+            {'camera_frame_id': 'camera_auto'},
+            {'video_device': dev_path} 
+        ]
+    )
+
+    gst_streamer = ExecuteProcess(
+        cmd=[f"sleep 2 ; gst-launch-1.0 rtspsrc location=rtsp://127.0.0.1:{port}/image_rtsp ! fakesink"],
+        cwd="/home",
+        shell=True
+    )
+
     return LaunchDescription([
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_launch_dir, 'camera_front.launch.py'))),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_launch_dir, 'camera_bottom.launch.py')))
+        LogInfo(msg=f"--- MIRA Auto-Launch: Detected {machine}, using {dev_path} ---"),
+        camera_node,
+        gst_streamer
     ])
